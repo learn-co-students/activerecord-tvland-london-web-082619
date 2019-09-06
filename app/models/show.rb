b00fcb031e2b6371940cc86d542b0d327d15ace1
Angus Morrison <contact@angus-morrison.com>
2019-09-06 11:34:06 +0100

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map { | actor | actor.full_name }
+    end
+
+    def build_network(call_letters:)
+        new_network = Network.find_or_create_by(call_letters: call_letters)
+        new_network.shows << self
+    end
 end
