25bc6eb7278e174559cb7201c578ce7536176abe
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-08 17:47:31 +0100

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,20 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say #returns 'Tyrion Lannister always says: A Lannister always pays his debts'
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(attributes)
+    new_show = Show.create(attributes)
+    self.show = new_show
+  end
+
+  def build_network(attributes)
+    new_network = Network.create(attributes)
+    self.show.network = new_network
+  end
+
+
 end
